bb24bc776f9f33455581811a75e05e9bbc874431
Segouta <christianbijvoets@gmail.com>
2018-01-27 16:28:31 +0100

    more code cleaning for better code hub

--- app/src/main/java/com/example/christian/pictured/SettingsActivity.java
+++ app/src/main/java/com/example/christian/pictured/SettingsActivity.java
@@ -49,41 +49,11 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
-    void showNotification(String title, String content) {
-
-        Uri notificationSound = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.notification);
-
-        long[] array = {3000};
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("default",
-                    "SnapThatChannel",
-                    NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription("Notifications for SnapThat");
-            mNotificationManager.createNotificationChannel(channel);
-        }
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
-                .setSmallIcon(R.drawable.new_icon) // notification icon
-                .setContentTitle(title) // title for notification
-                .setContentText(content)// message for notification
-                .setVibrate(array)
-                .setSound(notificationSound) // set alarm sound for notification
-                .setAutoCancel(true); // clear notification after click
-        Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
-        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(pi);
-        mNotificationManager.notify(0, mBuilder.build());
-    }
-
     @Override
     public void onClick(View v) {
         v.startAnimation(AnimationUtils.loadAnimation(this, R.anim.back_click));
         if (v.equals(back)) {
             this.onBackPressed();
-        }
-        if (v.equals(notification)) {
-            showNotification("A new present arrived!", "Click here to check it out!");
         }
     }
 }
